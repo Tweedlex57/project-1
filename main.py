19a88ddb185cc5631a16ac819afd3905f70339ae
Shevk <Sheasdasd@kdas.com>
2024-12-06 21:09:34 +1000

    first commit

--- main.py
+++ main.py
@@ -1,11 +1,69 @@
 import sqlite3
-con = sqlite3.connect('coffee.sqlite')
+import sys
 
-cur = con.cursor()
+from PyQt6 import uic
+from PyQt6.QtWidgets import QApplication
+from PyQt6.QtWidgets import QMainWindow, QTableWidgetItem
 
-result = cur.execute("""SELECT * FROM coffee""").fetchall()
 
-for i in result:
-    print(i)
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main.ui', self)
+        self.connection = sqlite3.connect("coffee.sqlite")
+        self.pushButton.clicked.connect(self.run)
+        self.initUI()
 
-con.close()
+    def initUI(self):
+        self.tableWidget.setColumnCount(7)
+        self.tableWidget.setRowCount(0)
+        res = cur.connection.cursor().execute("""SELECT * FROM coffee""").fetchall()
+        for i, row in enumerate(res):
+            self.tableWidget.setRowCount(
+                self.tableWidget.rowCount() + 1)
+            for j, elem in enumerate(row):
+                self.tableWidget.setItem(
+                    i, j, QTableWidgetItem(str(elem)))
+
+    def run(self):
+        self.AddWidget = AddWidget()
+        self.AddWidget.show()
+        self.close()
+
+    def closeEvent(self, event):
+        self.connection.close()
+
+
+class AddWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        self.connection = sqlite3.connect("coffee.sqlite")
+        self.pushButton.clicked.connect(self.run)
+        self.backButton.clicked.connect()
+
+    def run(self):
+        name = self.nameEdit.text()
+        step = self.stepEdit.text()
+        line = self.lineEdit.text()
+        description = self.descriptionEdit.text()
+        price = self.priceEdit.text()
+        package = self.packageEdit.text()
+        self.connection.cursor().execute(f'''INSERT INTO coffee(variety_name, degree_of_roasting, ground_in_grains, 
+        taste_description, price, package_volume) VALUES 
+                    (?, ?, ?, ?, ?, ?)''', (name, step, line, description, price, package,))
+
+    def back(self):
+        self.MyWidget = MyWidget()
+        self.MyWidget.show()
+        self.close()
+
+    def closeEvent(self, event):
+        self.connection.close()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec())
